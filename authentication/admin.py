59749f9655f310964a013b648b37ade941bd9215
Liaksej <tarusov.liaksej@gmail.com>
2023-12-02 16:33:54 +0100

    Add Django filters and update settings
    Introduced Django filters to the settings.py and added REST framework configurations for filter backends, search, ordering, and pagination options. Included `django_filters` in the installed apps. Moreover, carried out some minor clean-up tasks in urls.py and admin.py to improve code organization.

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,7 @@
+from allauth.account.models import EmailAddress
 from django.contrib import admin
+from rest_framework.authtoken.admin import TokenProxy
+
 
 # Register your models here.
+admin.site.unregister(TokenProxy)
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -59,6 +60,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_filters",
     "rest_framework",
     "rest_framework.authtoken",
     "rest_framework_simplejwt",
@@ -208,5 +210,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication"
-    ]
+    ],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
+    "SEARCH_PARAM": "q",
+    "ORDERING_PARAM": "order",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
 }
